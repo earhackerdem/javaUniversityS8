ebe05e0e425376ee77e6883749b503ae0d9972d6
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-03 23:16:17 -0500

    52 Creación de métodos en Java

--- src/operaciones/Aritmetica.java
+++ src/operaciones/Aritmetica.java
@@ -9,6 +9,11 @@ public class Aritmetica {
     //Metodo
     public void sumar() {
         int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+
+    public int sumarConRetorno() {
+        return a + b;
     }
 
 }
