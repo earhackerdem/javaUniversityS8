052ce867c4cdfae716faa7a571333b140421a332
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-05 17:42:13 -0500

    57 Alcance de variables

--- src/operaciones/Aritmetica.java
+++ src/operaciones/Aritmetica.java
@@ -19,7 +19,7 @@ public class Aritmetica {
 
     //Metodo
     public void sumar() {
-        int resultado = a + b;
+        int resultado = this.a + this.b;
         System.out.println("resultado = " + resultado);
     }
 
