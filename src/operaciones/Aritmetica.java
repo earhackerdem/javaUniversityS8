7b355c9700c7896ab1bd4dfc9579fb9b1d0668ad
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-05 17:17:47 -0500

    56 Sobrecarga de constructores

--- src/operaciones/Aritmetica.java
+++ src/operaciones/Aritmetica.java
@@ -5,10 +5,16 @@ public class Aritmetica {
     //Atributos de la clase
     int a; // el argumento a se asigna al atributo this.a
     int b;
-    
+
     //Constructor vacio
-    public Aritmetica(){
+    public Aritmetica() {
         System.out.println("Ejecutando constructor");
+    }
+
+    public Aritmetica(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.println("Ejecutando constructor argumentos");
     }
 
     //Metodo
--- src/operaciones/PruebaAritmetica.java
+++ src/operaciones/PruebaAritmetica.java
@@ -4,16 +4,12 @@ public class PruebaAritmetica {
 
     public static void main(String[] args) {
         Aritmetica aritmetica1 = new Aritmetica();
-        aritmetica1.a = 3;
-        aritmetica1.b = 2;
+        System.out.println("aritmetica1 a:" + aritmetica1.a);
+        System.out.println("aritmetica1 b:" + aritmetica1.b);
 
-        aritmetica1.sumar();
-
-        var resultado = aritmetica1.sumarConRetorno();
-        System.out.println("resultado = " + resultado);
-
-        resultado = aritmetica1.sumarConArgumentos(5, 8);
-        System.out.println("resultado usando argumentos = " + resultado);
+        Aritmetica aritmetica2 = new Aritmetica(5, 8);
+         System.out.println("aritmetica2 a:" + aritmetica2.a);
+        System.out.println("aritmetica2 b:" + aritmetica2.b);
 
     }
 }
